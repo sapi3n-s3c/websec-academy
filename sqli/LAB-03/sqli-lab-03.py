affbb71b179829e140f9c8f54d119a7d1cd4d1d9
T-Streets <tyler@featherandash.io>
2021-04-21 21:53:48 -0700

    sets up error handling for running script

--- sqli/LAB-03/sqli-lab-03.py
+++ sqli/LAB-03/sqli-lab-03.py
@@ -7,3 +7,13 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 proxies = {'http': '127.0.01:8080', 'https': '127.0.01:8080'}
 
+
+
+if __name__ == '__main__':
+    try:
+        url = sys.argv[1].strip()
+    except IndexError:
+        print(f'[-] Usage: {sys.argv[0]} <url>')
+        print(f'[-] Example {sys.argv[0]} www.example.com')
+
+    print('[+] Determining number of columns...')
