fd4d09bddb26226b2e4b3d1c5db57fd61777234d
techno-sapi3n <tyler.ceja5@gmail.com>
2021-04-29 21:50:46 -0700

    creates template for script | adds notes

---
+++ sqli/LAB-05/sqli-lab-05.py
@@ -0,0 +1,29 @@
+import requests
+import sys
+import urllib3
+import re
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'https://127.0.0.1:8080'}
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    try:
+        url = sys.argv[1].strip()
+    
+    except IndexError:
+        print(f'[-] Usage: {sys.argv[0]} <url>')
+        print(f'[-] Example: {sys.argv[0]} www.example.com')
+        sys.exit(-1)
+
